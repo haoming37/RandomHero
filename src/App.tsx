50f07cc815342b60beee48dedfbdb8cab76fba9d
Haoming <haoming37@gmail.com>
2023-10-31 21:13:54 +0900

    追加

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       "sigma",
       "winston",
       "zarya",
+			"ramattra",
     ];
     const selected = Math.floor(Math.random() * tank.length);
     setImage("tank/" + tank[selected] + ".png");
@@ -28,6 +29,8 @@ function App() {
       "mercy",
       "moira",
       "zenyatta",
+			"lifeweaver",
+			"illari",
     ];
     const selected = Math.floor(Math.random() * support.length);
     setImage("support/" + support[selected] + ".png");
